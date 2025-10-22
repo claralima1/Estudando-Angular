2270f6c059b3b673535ee382c09d46c992226257
Clara Lima <86741485+claralima1@users.noreply.github.com>
2025-10-22 11:27:29 +0000

    commit inicial

--- projeto/src/app/app.ts
+++ projeto/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FloatLabel } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FloatLabel],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
